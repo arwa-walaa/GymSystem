b0a0bdda45bd803c1d66411a5d411b5ee42105b1
arwa-walaa <84721941+arwa-walaa@users.noreply.github.com>
2025-10-07 20:14:43 +0300

    Models Creation

---
+++ GymSystemDAL/Entities/HealthRecord.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymSystemDAL.Entities
+{
+    internal class HealthRecord : BaseEntity
+    {
+     
+        public decimal Weight { get; set; }
+        public decimal Height { get; set; }
+
+        public string BloodType { get; set; }
+
+        public string? Note { get; set; }
+
+    }
+}
---
+++ GymSystemDAL/Entities/Trainer.cs
@@ -0,0 +1,16 @@
+﻿using GymSystemDAL.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymSystemDAL.Entities
+{
+    internal class Trainer : GymUser
+    {
+      
+        public Specialist Specialites { get; set; }
+
+    }
+}
