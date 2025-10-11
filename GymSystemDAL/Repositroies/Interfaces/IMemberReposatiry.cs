7f2b3072d08c8333e974d6941574854e62c40965
arwa-walaa <84721941+arwa-walaa@users.noreply.github.com>
2025-10-11 14:33:44 +0300

    Repository Design Pattern

---
+++ GymSystemDAL/Repositroies/Interfaces/IMemberReposatiry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymSystemDAL.Repositroies.Interfaces
+{
+    internal interface IMemberReposatiry
+    {
+        IEnumerable<Entities.Member> GetAll();
+
+        Entities.Member? GetById(int id);
+
+        int Add(Entities.Member member);
+
+        int Update(Entities.Member member);
+
+        int Delete(int id);
+    }
+}
