c33b678b4cf608cc888a9d83d273a37d3f27b44e
arwa-walaa <84721941+arwa-walaa@users.noreply.github.com>
2025-10-25 15:29:09 +0300

    Plan Controller [Edit]

--- GymSystemPL/Controllers/PlanController.cs
+++ GymSystemPL/Controllers/PlanController.cs
@@ -1,4 +1,6 @@
 ﻿using GymSystemBLL.Services.Interfaces;
+using GymSystemBLL.ViewModels;
+
 using Microsoft.AspNetCore.Mvc;
 
 namespace GymSystemPL.Controllers
@@ -33,6 +35,39 @@ namespace GymSystemPL.Controllers
 
 
 
+        }
+
+        public ActionResult Edit(int id)
+        {
+            if (id <= 0)
+            {
+                TempData["ErrorMessage"] = "Invalid Plan Id.";
+                return RedirectToAction(nameof(Index));
+            }
+            var plan = _planService.GetPlanToUpdate(id);
+            if (plan == null)
+            {
+                TempData["ErrorMessage"] = "Plan not found.";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(plan);
+        }
+        [HttpPost]
+        public ActionResult Edit([FromRoute] int id, UpdatePlanViewModel planViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("wrong Data", "Please correct the errors and try again.");
+                return View(planViewModel);
+            }
+            var updated = _planService.UpdatePlan(id, planViewModel);
+            if (!updated)
+            {
+                TempData["ErrorMessage"] = "Failed to update the plan.";
+                return RedirectToAction(nameof(Index));
+            }
+            TempData["SuccessMessage"] = "Plan updated successfully.";
+            return RedirectToAction(nameof(Index));
         }
 
     }
